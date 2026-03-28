6cfbb76dfe904fd8263ab619bd8bd952fa5fa601
Shravan Deva <devashravan7@gmail.com>
2026-03-28 14:06:07 +0530

    Add description and bringup packages
    Signed-off-by: Shravan Deva <devashravan7@gmail.com>

---
+++ chitrak_bringup/launch/display.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+import os
+
+
+def generate_launch_description():
+
+    # Path to URDF file
+    pkg_path = FindPackageShare("chitrak_description").find("chitrak_description")
+    urdf_path = os.path.join(pkg_path, "urdf", "chitrak.urdf")
+
+    # Path to RViz config
+    bringup_pkg_path = FindPackageShare("chitrak_bringup").find("chitrak_bringup")
+    rviz_config_path = os.path.join(bringup_pkg_path, "config", "display.rviz")
+
+    with open(urdf_path, 'r') as f:
+        robot_description_content = f.read()
+
+    robot_description = {"robot_description": robot_description_content}
+
+    # Robot State Publisher
+    rsp_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description]
+    )
+
+    # Joint State Publisher (GUI for sliders)
+    jsp_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui"
+    )
+
+    # RViz
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_path]
+    )
+
+    return LaunchDescription([
+        rsp_node,
+        jsp_node,
+        rviz_node
+    ])
---
+++ chitrak_description/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'chitrak_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # Install URDF files
+        ('share/' + package_name + '/urdf', glob('urdf/*.urdf')),
+        # Install Mesh files
+        ('share/' + package_name + '/meshes', glob('meshes/*.STL')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shravan',
+    maintainer_email='devashravan7@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
